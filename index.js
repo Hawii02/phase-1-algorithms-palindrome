4a3029529c5ea8523373b2788ba3b5a45540b077
Hawii02 <leon.onditi@student.moringaschool.com>
2024-01-10 21:23:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = word.split('').reverse().join('');
+  if (word.toLowerCase() === reversedWord.toLowerCase()) {
+  return true;
+  }else {
+  return false;
+  } 
+
+  console.log(isPalindrome("tot"));
 }
 
 /* 
   Add your pseudocode here
+if the string reads same forwards and backwards the input should
+  return true
+else
+  return false if it is not a palindrome
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
